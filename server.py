721fee20f47b88f0052351edfc9ebb8c2fd8247b
James Bonifacio <jplbonif@uwaterloo.ca>
2022-03-16 12:44:13 -0400

    Save blinks in DB

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_mysqldb import MySQL
+
+import sys
 
 
 app = Flask(__name__)
@@ -26,6 +28,33 @@ def index():
     response = cursor.fetchall()
 
     return {"response": response}
+
+@app.route('/blinks', methods=['GET', 'POST'])
+def show_all_blinks():
+    if request.method == 'GET':
+        query = "SELECT * FROM blinks"
+
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        response = cursor.fetchall()
+
+        return {"response": response}
+
+    else:
+        request_data = request.get_json(force=True)
+
+        rising_time = request_data['rising_time']
+        falling_time = request_data['falling_time']
+
+        query = f'INSERT INTO blinks (rising_time, falling_time) VALUES ({rising_time}, {falling_time})'
+
+        connection = mysql.connection
+        cursor = connection.cursor()
+        cursor.execute(query)
+        connection.commit()
+
+        return {"success": True}
+
 
 @app.route('/user/<username>')
 def show_user_profile(username):
